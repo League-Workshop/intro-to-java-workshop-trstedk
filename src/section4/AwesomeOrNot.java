b705ad2df22e924519343182ca9d096255168aa0
League <League@AdminisorsiMac2.attlocal.net>
2018-04-12 14:52:57 -0700

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,18 +2,25 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
+
+Random randgen= new Random();
+int x= randgen.nextInt(100);
 	// 3. Print out this variable
-
+System.out.println(x);
 	// 4. Get the user to enter something that they think is awesome
-
+String response=JOptionPane.showInputDialog(null,"What is Awesome?");
 	// 5. If the random number is 0
-
+if(x==0) {
 	// -- tell the user whatever they entered is awesome!
 
 	// 6. If the random number is 1
@@ -29,5 +36,5 @@ public class AwesomeOrNot {
 	// -- write your own answer
 
 }
-
-
+}
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,28 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int x=0;
 		// 2.  Ask the user a question 
-		
+		String name=JOptionPane.showInputDialog(null,"what is the world based off of");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		String str="starbucks frappucino";
+		if(str.equals("starbucks frappucino")) {
 		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
+		// -- add one to their score
+			x = x + 1;
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		name=JOptionPane.showInputDialog(null,"why do we have to keep making questions");
+		str="I do not know";
+		if(str.equals("I dont know")) {
 		// 6.  After all the questions have been asked, print the user's score 
-		
 	}
-}
+	}
+	}
+
